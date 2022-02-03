7c194f58ad32d70ded8c46567a45af4b9cf00930
Zaland Khan <zalandkhan1@gmail.com>
2022-02-03 14:03:26 -0800

    Update eslint

--- UI/.eslintrc.js
+++ UI/.eslintrc.js
@@ -12,13 +12,11 @@ module.exports = {
     ecmaVersion: 13,
     sourceType: "module",
   },
-  plugins: [
-    "react",
-    "@typescript-eslint"
-  ],
+  plugins: ["react", "@typescript-eslint"],
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   rules: {
     "react/display-name": "off",
