3d0ffd2934ef0049b72fe210614e1c204ec3b025
Zaland Khan <zalandkhan1@gmail.com>
2022-02-02 17:47:54 -0800

    Add eslint

---
+++ UI/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    "react/display-name": "off",
+  },
+};
